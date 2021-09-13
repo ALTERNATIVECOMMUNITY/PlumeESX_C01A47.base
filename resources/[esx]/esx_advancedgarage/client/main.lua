4d732bded38dc8a8de225f6f777b3e0b66563b95
tnwrestler360 <47567360+tnwrestler360@users.noreply.github.com>
2021-09-13 16:54:50 -0700

    Vehicle dmg to Garage
    Vehicle dmg is now "properly" applied when you pull the car back out and we save the tire state.

--- resources/[esx]/esx_advancedgarage/client/main.lua
+++ resources/[esx]/esx_advancedgarage/client/main.lua
@@ -820,10 +820,9 @@ function StoreOwnedPoliceMenu()
 		local coords = GetEntityCoords(playerPed)
 		local vehicle = GetVehiclePedIsIn(playerPed, false)
 		local vehicleProps = ESX.Game.GetVehicleProperties(vehicle)
-		local current = GetPlayersLastVehicle(GetPlayerPed(-1), true)
-		local engineHealth = GetVehicleEngineHealth(current)
 		local plate = vehicleProps.plate
-
+		local tyre = {IsVehicleTyreBurst(vehicle, 0, false),IsVehicleTyreBurst(vehicle, 1, false),IsVehicleTyreBurst(vehicle, 4, false),IsVehicleTyreBurst(vehicle, 5, false)}
+		vehicleProps['tyre'] = tyre
 		ESX.TriggerServerCallback('esx_advancedgarage:storeVehicle', function(valid)
 			if valid then
 					StoreVehicle(vehicle, vehicleProps, currentLoc)	
@@ -2225,34 +2224,12 @@ function StoreVehicle(vehicle, vehicleProps, location)
 	exports['mythic_notify']:DoHudText('inform', 'You parked your car!')
 end
 
--- Spawn Vehicles
-function SpawnVehicle(vehicle, plate, fuel)
-	ESX.Game.SpawnVehicle(vehicle.model, this_Garage.Spawner, this_Garage.Heading, function(callback_vehicle)
-		ESX.Game.SetVehicleProperties(callback_vehicle, vehicle)
-		SetVehRadioStation(callback_vehicle, "OFF")
-		--SetVehicleFixed(callback_vehicle)
-		--SetVehicleDeformationFixed(callback_vehicle)
-		SetVehicleUndriveable(callback_vehicle, false)
-		SetVehicleEngineOn(callback_vehicle, true, true)
-		local carplate = GetVehicleNumberPlateText(callback_vehicle)
-		table.insert(vehInstance, {vehicleentity = callback_vehicle, plate = carplate})
-		if Config.Main.LegacyFuel then
-			exports['LegacyFuel']:SetFuel(callback_vehicle, fuel)
-		end
-		TaskWarpPedIntoVehicle(GetPlayerPed(-1), callback_vehicle, -1)
-		
-		exports["onyxLocksystem"]:givePlayerKeys(carplate)
-	end)
-
-	TriggerServerEvent('esx_advancedgarage:setVehicleState', plate, false)
-end
-
 function SpawnVehicle2(vehicle, plate, fuel, vector, heading)
 	ESX.Game.SpawnVehicle(vehicle.model, vector, heading, function(callback_vehicle)
 		ESX.Game.SetVehicleProperties(callback_vehicle, vehicle)
 		SetVehRadioStation(callback_vehicle, "OFF")
-		--SetVehicleFixed(callback_vehicle)
-		--SetVehicleDeformationFixed(callback_vehicle)
+		SetVehicleBodyHealth(callback_vehicle, vehicle.bodyHealth)
+		doCarDamages(vehicle.engineHealth, vehicle.bodyHealth, callback_vehicle, vehicle.tyre)
 		SetVehicleUndriveable(callback_vehicle, false)
 		SetVehicleEngineOn(callback_vehicle, true, true)
 		local carplate = GetVehicleNumberPlateText(callback_vehicle)
@@ -2264,8 +2241,61 @@ function SpawnVehicle2(vehicle, plate, fuel, vector, heading)
 		exports["onyxLocksystem"]:givePlayerKeys(carplate)
 		exports['mythic_notify']:DoHudText('inform', 'You pulled out your vehicle!')
 	end)
-	TriggerServerEvent('esx_advancedgarage:setVehicleState', plate, 0)
-	
+	TriggerServerEvent('esx_advancedgarage:setVehicleState', plate, 0)	
+end
+
+function doCarDamages(eh, bh, veh, tyres)
+	smash = false
+	damageOutside = false
+    if eh and bh and tyres then
+        local engine = eh + 0.0
+        local body = bh + 0.0
+        if engine < 200.0 then
+            engine = 200.0
+        end
+
+        if body < 150.0 then
+            body = 150.0
+        end
+        if body < 890.0 then
+            smash = true
+        end
+
+        if body < 600.0 then
+            damageOutside = true
+        end
+
+        local currentVehicle = (veh and IsEntityAVehicle(veh)) and veh or GetVehiclePedIsIn(PlayerPedId(), false)
+
+        Citizen.Wait(100)
+        SetVehicleEngineHealth(currentVehicle, engine)
+        if smash then
+            SmashVehicleWindow(currentVehicle, 0)
+            SmashVehicleWindow(currentVehicle, 1)
+            SmashVehicleWindow(currentVehicle, 2)
+            SmashVehicleWindow(currentVehicle, 3)
+            SmashVehicleWindow(currentVehicle, 4)
+        end
+        if damageOutside then
+            SetVehicleDoorBroken(currentVehicle, 0, true)
+            SetVehicleDoorBroken(currentVehicle, 1, true)
+            SetVehicleDoorBroken(currentVehicle, 6, true)
+            SetVehicleDoorBroken(currentVehicle, 5, true)
+            SetVehicleDoorBroken(currentVehicle, 4, true)
+        end
+		
+        if tyres then
+			local toPop = {0,1,4,5}
+            for i, t in pairs(tyres) do
+				if t == 1 then
+					SetVehicleTyreBurst(currentVehicle, toPop[i], true, 1000.0)	
+				end
+			end
+        end
+        if body < 1000 then
+            SetVehicleBodyHealth(currentVehicle, body)
+        end
+    end
 end
 
 -- Check Vehicles
